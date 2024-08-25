1a2abb9368cb69b5df763c3a93a35af34e7b0e7a
John <jakajohn@web.de>
2024-08-25 13:54:07 +0200

    feature: implemate notes section in the DetailsScreen with two components NoteList and NoteCard. It has also a date info and delte button to delete the note.

--- Screens/DetailsScreen.tsx
+++ Screens/DetailsScreen.tsx
@@ -12,7 +12,14 @@ import {
 } from "react-native";
 import React, { useEffect, useState, useRef } from "react";
 import { FIREBASE_FIRESTORE } from "../firebaseConfig";
-import { getDoc, doc, updateDoc, DocumentData } from "firebase/firestore";
+import {
+  getDoc,
+  updateDoc,
+  DocumentData,
+  query,
+  getDocs,
+  collection,
+} from "firebase/firestore";
 import { User as User } from "firebase/auth";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { PieChart } from "react-native-chart-kit";
@@ -28,6 +35,7 @@ import EarningsCalculatorCard from "../components/EarningsCalculatorCard";
 import { fetchProjectData } from "../components/services/ChartDataService";
 import { getChartData } from "../components/PieChartProgressUtils";
 import NoteModal from "../components/NoteModal";
+import NoteList from "../components/NoteList";
 import { useStore } from "../components/TimeTrackingState";
 import DigitalClock from "../components/DigitalClock";
 //import { useAppState } from "../AppStateStore";
@@ -47,10 +55,10 @@ type RootStackParamList = {
   Details: { projectId: string; projectName: string };
 };
 
-interface ChartData {
-  name: string;
-  hours: number;
-  color: string;
+interface Note {
+  id: string;
+  comment: string;
+  createdAt: Date;
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -58,122 +66,59 @@ interface ChartData {
 const DetailsScreen: React.FC = () => {
   const route = useRoute<DetailsScreenRouteProp>();
   const navigation = useNavigation();
-  const { projectId = "No ID received", projectName = "No Name received" } =
-    route.params || {};
+  const { projectId = "No ID received" } = route.params || {};
   const { setProjectId } = useStore();
+
+  // states to manage the NoteCard Value fetching
+  const [note, setNotes] = useState<Note[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     setProjectId(projectId);
   }, [projectId, setProjectId]);
 
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const notesQuery = query(
+          collection(
+            FIREBASE_FIRESTORE,
+            `Services/AczkjyWoOxdPAIRVxjy3/Projects/${projectId}/Notes`
+          )
+        );
+        const notesSnapshot = await getDocs(notesQuery);
+
+        if (!notesSnapshot.empty) {
+          const fetchedNotes: Note[] = notesSnapshot.docs.map((doc) => {
+            const data = doc.data();
+            return {
+              id: doc.id,
+              comment: data.comment,
+              createdAt: data.createdAt.toDate(),
+            };
+          });
+          setNotes(fetchedNotes);
+        } else {
+          console.log("No notes found for this project.");
+        }
+      } catch (error) {
+        console.error("Error fetching notes: ", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchNotes();
+  }, [projectId]);
+
   // const [project, setProject] = useState<any>(null);
   // console.log("Project ID:", projectsId);
   // console.log("Project Name:", projectName);
 
-  const [hourlyRate, setHourlyRate] = useState<string>("0.00");
-  const [totalEarnings, setTotalEarnings] = useState<number>(0);
+  //const [hourlyRate, setHourlyRate] = useState<string>("0.00");
+  //const [totalEarnings, setTotalEarnings] = useState<number>(0);
   //const { projectData, updateElapsedTime } = useAppState();
   // const [appState, setAppState] = useState(AppState.currentState);
-
-  const [projectData, setProjectData] = useState<DocumentData | null>(null);
-  const [chartData, setChartData] = useState<ChartData[]>([]);
-  const [loading, setLoading] = useState(true);
-
-  const fetchProjectData = async (projectId: string) => {
-    try {
-      const docRef = doc(
-        FIREBASE_FIRESTORE,
-        "Services",
-        "AczkjyWoOxdPAIRVxjy3",
-        "Projects",
-        projectId
-      );
-      const docSnap = await getDoc(docRef);
-      if (docSnap.exists()) {
-        return docSnap.data();
-      } else {
-        console.warn("No such document for projectId:", projectId);
-        return null;
-      }
-    } catch (error) {
-      console.error("Error fetching project data:", error);
-      throw error; // optional, to handle it where the function is called
-    }
-  };
-
-  /*useEffect(() => {
-    const fetchProject = async () => {
-      try {
-        const docRef = doc(
-          FIREBASE_FIRESTORE,
-          "Services",
-          "AczkjyWoOxdPAIRVxjy3",
-          "Projects",
-          projectsId
-        );
-        const docSnap = await getDoc(docRef);
-        if (docSnap.exists()) {
-          setProject(docSnap.data());
-          setHourlyRate(docSnap.data().hourlyRate.toString());
-          setTotalEarnings(docSnap.data().totalEarnings || 0);
-        } else {
-          console.warn("No such document!");
-        }
-      } catch (error) {
-        console.error("Error fetching project: ", error);
-      }
-    };
-
-    fetchProject();
-  }, [projectsId]); */
-
-  /* const saveTrackingData = async (
-    startTime: string | null,
-    endTime: string | null,
-    elapsedTime: number,
-    isTracking: boolean
-  ) => {
-    try {
-      const projectDocRef = doc(
-        FIREBASE_FIRESTORE,
-        "Services",
-        "AczkjyWoOxdPAIRVxjy3",
-        "Projects",
-        projectsId
-      );
-      await updateDoc(projectDocRef, {
-        startTime,
-        endTime,
-        elapsedTime,
-        totalEarnings,
-        hourlyRate: parseFloat(hourlyRate),
-        isTracking,
-      });
-      console.log("Tracking data saved successfully");
-    } catch (error) {
-      console.error("Error saving tracking data:", error);
-    }
-  };
-
-  const formatElapsedTime = (seconds: number) => {
-    const h = Math.floor(seconds / 3600);
-    const m = Math.floor((seconds % 3600) / 60);
-    const s = Math.floor(seconds % 60);
-    return `${h.toString().padStart(2, "0")}:${m
-      .toString()
-      .padStart(2, "0")}:${s.toString().padStart(2, "0")}`;
-  }; */
-
-  /*useEffect(() => {
-    const loadData = async () => {
-      const projectData = await fetchProjectData(projectId);
-      if (projectData) {
-        setChartData(getChartData(projectData));
-      }
-    };
-
-    loadData();
-  }, [projectId]); */
 
   /*
   // function to show the chart-data
@@ -200,148 +145,6 @@ const DetailsScreen: React.FC = () => {
 
     loadData();
   }, [projectsId]);  */
-
-  /*useEffect(() => {
-    let timer: NodeJS.Timeout | null = null;
-    if (isTracking && startTime !== "N/A" && startTime !== null) {
-      timer = setInterval(() => {
-        setElapsedTime((prev) => prev + 1);
-      }, 1000);
-    }
-    return () => {
-      if (timer) {
-        clearInterval(timer);
-      }
-    };
-  }, [isTracking, startTime]);
-
-  const saveTrackingData = async () => {
-    try {
-      if (startTime !== null && startTime !== "N/A") {
-        const projectDocRef = doc(
-          FIREBASE_FIRESTORE,
-          "Services",
-          "AczkjyWoOxdPAIRVxjy3",
-          "Projects",
-          projectsId
-        );
-        await updateDoc(projectDocRef, {
-          startTime,
-          endTime,
-          pauseTime,
-          elapsedTime,
-          totalEarnings,
-          hourlyRate: parseFloat(hourlyRate),
-        });
-        console.log("Tracking data saved successfully");
-      } else {
-        console.log("Cannot save tracking data: startTime is not initialized");
-      }
-    } catch (error) {
-      console.error("Error saving tracking data:", error);
-    }
-  }; */
-
-  /*
-  // function to handle start, pause, and end tracking
-  const handleStart = () => {
-    if (!isTracking) {
-      const now = new Date();
-      setStartTime(now.toISOString());
-      setIsTracking(true);
-      saveTrackingData();
-    }
-  };
-
-  const handlePause = () => {
-    setIsTracking(false);
-    setPauseTime(new Date().toISOString());
-    saveTrackingData();
-  };
-
-  const handleEnd = () => {
-    setIsTracking(false);
-    const now = new Date();
-    const elapsed =
-      startTime !== "N/A"
-        ? (new Date(now).getTime() - new Date(startTime as string).getTime()) /
-          1000
-        : 0;
-    setEndTime(now.toISOString());
-    setElapsedTime(elapsed);
-    saveTrackingData();
-  };
-
-  // function to format elapsed time
-  const formatElapsedTime = (seconds: number) => {
-    const h = Math.floor(seconds / 3600);
-    const m = Math.floor((seconds % 3600) / 60);
-    const s = Math.floor(seconds % 60);
-    return `${h.toString().padStart(2, "0")}:${m.toString().padStart(2, "0")}:${s.toString().padStart(2, "0")}`;
-  };
-
-  const handlePress = async () => {
-    try {
-      const rate = parseFloat(hourlyRate);
-      if (!isNaN(rate)) {
-        setSavedHourlyRate(rate);
-        setHourlyRate("");
-
-        const projectDocRef = doc(
-          FIREBASE_FIRESTORE,
-          "Services",
-          "AczkjyWoOxdPAIRVxjy3",
-          "Projects",
-          projectsId
-        );
-
-        await updateDoc(projectDocRef, {
-          hourlyRate: rate,
-        });
-      } else {
-        console.error("Invalid hourly rate:", hourlyRate);
-      }
-    } catch (error) {
-      console.error("Error saving hourly rate:", error);
-    }
-  }; */
-
-  /*useEffect(() => {
-    const fetchProject = async () => {
-      const docRef = doc(
-        FIREBASE_FIRESTORE,
-        "Services",
-        "AczkjyWoOxdPAIRVxjy3",
-        "Projects",
-        projectsId
-      );
-
-      const docSnap = await getDoc(docRef);
-      if (docSnap.exists()) {
-        setProject(docSnap.data());
-        setElapsedTime(docSnap.data().elapsedTime || 0);
-      }
-    };
-
-    fetchProject();
-  }, [projectsId]); */
-
-  /* const data = [
-    {
-      name: "Erreichte Stunden",
-      hours: elapsedTime / 3600,
-      color: "#77D353",
-      legendFontColor: "#7F7F7F",
-      legendFontSize: 15,
-    },
-    {
-      name: "Verbleibende Stunden",
-      hours: (project ? project.plannedHours : 0) - elapsedTime / 3600,
-      color: "#F6CE46",
-      legendFontColor: "#7F7F7F",
-      legendFontSize: 15,
-    },
-  ];  */
 
   /*
   // loading indicator when data is loading from firebase
@@ -372,72 +175,11 @@ const DetailsScreen: React.FC = () => {
         <TimeTrackerCard />
         {/* Earn-Calculator-Card */}
         <EarningsCalculatorCard />
-        <Text style={{ color: "white" }}>
-          Project ID: {projectId ? projectId : "No ID received"}
-        </Text>
-        <Text style={{ color: "white" }}>Project Name: {projectName}</Text>
 
         {/* Success Chart */}
-        <View
-          style={{
-            marginBottom: 20,
-            backgroundColor: "#191919",
-            borderRadius: 8,
-            borderWidth: 1,
-            borderColor: "aqua",
-            padding: 5,
-          }}
-        >
-          <Text
-            style={{
-              fontFamily: "MPLUSLatin_Bold",
-              fontSize: 25,
-              color: "white",
-              marginBottom: 10,
-            }}
-          >
-            Success Chart
-          </Text>
-          <PieChart
-            data={chartData}
-            width={Dimensions.get("window").width}
-            height={160}
-            chartConfig={{
-              backgroundGradientFrom: "#1E2923",
-              backgroundGradientTo: "#08130D",
-              color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
-              strokeWidth: 2,
-              barPercentage: 0.5,
-              useShadowColorFromDataset: false,
-            }}
-            accessor="hours"
-            backgroundColor="transparent"
-            paddingLeft="15"
-            absolute
-          />
-        </View>
-        {/* Success Chart */}
-        <View
-          style={{
-            marginBottom: 20,
-            backgroundColor: "#191919",
-            borderRadius: 8,
-            borderWidth: 1,
-            borderColor: "aqua",
-            padding: 5,
-          }}
-        >
-          <Text
-            style={{
-              fontFamily: "MPLUSLatin_Bold",
-              fontSize: 25,
-              color: "white",
-              marginBottom: 10,
-            }}
-          >
-            Your Notes from {projectName}
-          </Text>
-        </View>
+
+        {/* Notes Card */}
+        <NoteList projectId={projectId} />
       </View>
     </ScrollView>
   );
---
+++ components/NoteList.tsx
@@ -0,0 +1,129 @@
+////////////////////////////Note List Component////////////////////////////////
+
+import React, { useState, useEffect } from "react";
+import { ScrollView, View, Text, ActivityIndicator } from "react-native";
+import { collection, query, getDocs, DocumentData } from "firebase/firestore";
+
+import NoteCard from "./NoteCard";
+import { FIREBASE_FIRESTORE } from "../firebaseConfig";
+
+///////////////////////////////////////////////////////////////////////////////
+interface Note {
+  id: string;
+  comment: string;
+  createdAt: Date;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+const NoteList: React.FC<{ projectId: string }> = ({ projectId }) => {
+  // state to manage the  useEffect to show the notes
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  // state to manage the loading Indicator
+  const [loading, setLoading] = useState<boolean>(true);
+
+  // useEffect to fetch the notes from Firestore with snapshot
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const notesQuery = query(
+          collection(
+            FIREBASE_FIRESTORE,
+            `Services/AczkjyWoOxdPAIRVxjy3/Projects/${projectId}/Notes`
+          )
+        );
+        const notesSnapshot = await getDocs(notesQuery);
+
+        if (!notesSnapshot.empty) {
+          const fetchedNotes: Note[] = notesSnapshot.docs.map((doc) => {
+            const data = doc.data() as DocumentData;
+            return {
+              id: doc.id,
+              comment: data.comment,
+              createdAt: data.createdAt.toDate(),
+            };
+          });
+          setNotes(fetchedNotes);
+        } else {
+          console.log("No notes found for this project.");
+        }
+      } catch (error) {
+        console.error("Error fetching notes: ", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchNotes();
+  }, [projectId]);
+
+  // function to handle note deletion
+  const handleDeleteNote = (noteId: string) => {
+    setNotes((prevNotes) => prevNotes.filter((note) => note.id !== noteId));
+  };
+
+  if (loading) {
+    return (
+      <View>
+        <ActivityIndicator size="large" color="blue" />
+      </View>
+    );
+  }
+
+  return (
+    <ScrollView>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <View
+          style={{
+            width: "100%",
+
+            marginBottom: 20,
+            backgroundColor: "#191919",
+            borderWidth: 1,
+            borderColor: "aqua",
+            borderRadius: 8,
+            padding: 20,
+            alignItems: "center",
+            overflow: "hidden",
+            flexShrink: 1,
+            flexGrow: 0,
+          }}
+        >
+          <Text
+            style={{
+              fontFamily: "MPLUSLatin_Bold",
+              fontSize: 25,
+              color: "white",
+              marginBottom: 40,
+            }}
+          >
+            Your Notes
+          </Text>
+          {notes.length > 0 ? (
+            notes.map((note) => (
+              <NoteCard
+                key={note.id}
+                note={note}
+                projectId={projectId}
+                onDelete={handleDeleteNote}
+              />
+            ))
+          ) : (
+            <Text
+              style={{
+                textAlign: "center",
+                color: "white",
+                fontSize: 18,
+                fontFamily: "MPLUSLatin_ExtraLight",
+              }}
+            >
+              You haven't any notes for this project yet.
+            </Text>
+          )}
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+export default NoteList;
