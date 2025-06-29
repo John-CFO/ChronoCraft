5a2792c8976bd7df0a7bf6cc796b478dc83550ca
John <john_schraps@web.de>
2025-06-29 22:53:34 +0200

    feat: add fickering animation if the deathline is arrived

--- components/ProgressCard.tsx
+++ components/ProgressCard.tsx
@@ -14,6 +14,7 @@ import {
   Alert,
   useWindowDimensions,
   Animated,
+  Easing,
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { LinearGradient } from "expo-linear-gradient";
@@ -85,9 +86,11 @@ const ProgressCard: React.FC<ProgressCardProps> = React.memo(
     ).current;
 
     // calculate progress with memoization
-    const progress = useMemo(() => {
-      return maxSeconds > 0 ? Math.min((timer / maxSeconds) * 100, 100) : 0;
+    const progressRaw = useMemo(() => {
+      return maxSeconds > 0 ? timer / maxSeconds : 0;
     }, [timer, maxSeconds]);
+    const progress = Math.min(progressRaw, 1); // 0 bis 1 → für Animated
+    const progressValue = Math.min(progressRaw * 100, 100); // 0 bis 100 → für CircularProgress
 
     // input state
     const [inputMaxWorkHours, setInputMaxWorkHours] = useState("");
@@ -145,6 +148,31 @@ const ProgressCard: React.FC<ProgressCardProps> = React.memo(
         useNativeDriver: false,
       }).start();
     }, [progress]);
+
+    // deathline arrived animation
+    const blinkOpacity = useRef(new Animated.Value(0)).current;
+    // hook to animate the progress indicator on deathline with flickering
+    useEffect(() => {
+      if (progressValue >= 100) {
+        Animated.loop(
+          Animated.sequence([
+            Animated.timing(blinkOpacity, {
+              toValue: 1,
+              duration: 200,
+              useNativeDriver: true,
+            }),
+            Animated.timing(blinkOpacity, {
+              toValue: 0,
+              duration: 200,
+              useNativeDriver: true,
+            }),
+          ])
+        ).start();
+      } else {
+        blinkOpacity.stopAnimation();
+        blinkOpacity.setValue(0);
+      }
+    }, [progressValue]);
 
     return (
       <View
@@ -253,26 +281,69 @@ const ProgressCard: React.FC<ProgressCardProps> = React.memo(
             </Text>
           </LinearGradient>
         </TouchableOpacity>
-        {/* ProgressCard */}
-        <CircularProgress
-          value={progress}
-          radius={100}
-          maxValue={100}
-          progressValueColor="white"
-          progressValueFontSize={42}
-          progressValueStyle={{
-            fontWeight: "bold",
-            fontFamily: "MPLUSLatin_ExtraLight",
+
+        {/* ProgressChart */}
+        <View
+          style={{
+            position: "relative",
+            width: 200,
+            height: 200,
+            justifyContent: "center",
+            alignItems: "center",
           }}
-          activeStrokeWidth={15}
-          inActiveStrokeWidth={15}
-          inActiveStrokeOpacity={0.3}
-          activeStrokeColor="#00e0ff"
-          activeStrokeSecondaryColor="#00ff6a"
-          inActiveStrokeColor="#333"
-          dashedStrokeConfig={{ count: 50, width: 4 }}
-          duration={0}
-        />
+        >
+          {/* BackgroundRing */}
+          <CircularProgress
+            value={100}
+            radius={100}
+            activeStrokeWidth={15}
+            inActiveStrokeWidth={15}
+            activeStrokeColor="#1e1e1e"
+            inActiveStrokeColor="#1e1e1e"
+            dashedStrokeConfig={{ count: 50, width: 4 }}
+            progressValueColor="transparent"
+            duration={0}
+          />
+          {/* ProgressRing */}
+          <View style={{ position: "absolute", top: 0, left: 0 }}>
+            <CircularProgress
+              value={progressValue}
+              radius={100}
+              activeStrokeWidth={15}
+              inActiveStrokeWidth={15}
+              // if progress arrives at 100%, change color
+              activeStrokeColor={progressValue < 100 ? "#00f7f7" : "#ff0000"}
+              dashedStrokeConfig={{ count: 50, width: 4 }}
+              inActiveStrokeColor="transparent"
+              progressValueColor="#ffffff"
+              duration={1000}
+            />
+          </View>
+          {/* flickering stroke animated ProgressRing */}
+          {progressValue >= 100 && (
+            <Animated.View
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                opacity: blinkOpacity,
+              }}
+            >
+              <CircularProgress
+                value={progressValue}
+                radius={100}
+                activeStrokeWidth={15}
+                inActiveStrokeWidth={15}
+                activeStrokeColor="#ffffff"
+                inActiveStrokeColor="transparent"
+                dashedStrokeConfig={{ count: 50, width: 4 }}
+                progressValueColor="transparent"
+                duration={0}
+              />
+            </Animated.View>
+          )}
+        </View>
+
         {/* Result Text */}
         {maxSeconds > 0 && timer > 0 && (
           <Text
