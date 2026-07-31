3e7ae62015833186c38180896980dc1883b935ef
John <john_schraps@web.de>
2026-07-31 17:05:14 +0200

    chore: remove debugging logs and CI debug build after environment fix

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,18 +10,6 @@ import * as admin from "firebase-admin";
 
 //////////////////////////////////////////////////////////////////////
 
-console.log("[FUNCTIONS DEBUG] index.ts loaded");
-console.log("[FUNCTIONS DEBUG] NODE_ENV:", process.env.NODE_ENV);
-console.log("[FUNCTIONS DEBUG] GCLOUD_PROJECT:", process.env.GCLOUD_PROJECT);
-console.log(
-  "[FUNCTIONS DEBUG] FIREBASE_PROJECT:",
-  process.env.FIREBASE_PROJECT,
-);
-console.log(
-  "[FUNCTIONS DEBUG] FIREBASE_AUTH_EMULATOR_HOST:",
-  process.env.FIREBASE_AUTH_EMULATOR_HOST,
-);
-
 setGlobalOptions({
   region: "us-central1",
   maxInstances: 10,
@@ -71,42 +59,30 @@ import {
 import { disableTotpHandler } from "./functions/disableTotp.function";
 
 // export functions
-console.log("[FUNCTIONS DEBUG] registering exports");
 export const authValidatorFunction = authValidator;
 
-console.log("[FUNCTIONS DEBUG] authValidatorFunction registered");
 export const projectsAndWorkValidatorFunction = projectsAndWorkValidator;
 
-console.log("[FUNCTIONS DEBUG] projectsAndWorkValidatorFunction registered");
 export const secureDeleteFunction = secureDelete;
 
-console.log("[FUNCTIONS DEBUG] secureDeleteFunction registered");
 export const checkTotpStatus = onCall({ cors: true }, checkTotpStatusHandler);
 
-console.log("[FUNCTIONS DEBUG] checkTotpStatus registered");
 export const disableTotp = onCall({ cors: true }, disableTotpHandler);
 
-console.log("[FUNCTIONS DEBUG] disableTotp registered");
 export const createTotpSecret = onCall({ cors: true }, createTotpSecretHandler);
 
-console.log("[FUNCTIONS DEBUG] createTotpSecret registered");
 export const verifyTotpToken = onCall({ cors: true }, verifyTotpTokenHandler);
 
-console.log("[FUNCTIONS DEBUG] verifyTotpToken registered");
 export const verifyTotpLogin = onCall({ cors: true }, verifyTotpLoginHandler);
 
-console.log("[FUNCTIONS DEBUG] verifyTotpLogin registered");
 export const deleteUserData = onCall({ cors: true }, deleteUserDataHandler);
 
-console.log("[FUNCTIONS DEBUG] deleteUserData registered");
 export const requestPasswordResetFunction = onCall(
   { cors: true },
   requestPasswordReset,
 );
 
-console.log("[FUNCTIONS DEBUG] requestPasswordResetFunction registered");
 export const registerPushTokenFunction = onCall(
   { cors: true },
   registerPushToken,
 );
-console.log("[FUNCTIONS DEBUG] registerPushTokenFunction registered");
